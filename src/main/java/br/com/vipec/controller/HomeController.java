ec61110b813548a079ce01222966770e0a42c36d
Michael de Matos <michael.m.cc@hotmail.com>
2020-01-29 07:47:30 -0300

    Novos logos;  Dashboard

--- src/main/java/br/com/vipec/controller/HomeController.java
+++ src/main/java/br/com/vipec/controller/HomeController.java
@@ -1,17 +1,28 @@
 package br.com.vipec.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
  * HomeController
  */
 @Controller
+@RequestMapping(path = "/")
 public class HomeController {
-    @GetMapping("/")
+    @GetMapping
     public String home() {
 
         return "redirect:/usuario/cadastrar";
     }
+
+    @GetMapping("dashboard")
+    public String showLoginForm(Model model) {
+
+        return "dashboard";
+    }
+
+
     
 }
